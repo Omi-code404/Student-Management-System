ade8659bbc3a3c09b0be83bd23c5d672eef830c7
omi-code404 <omi.bn.ctg2004@gmail.com>
2026-01-06 09:48:00 +0600

    Initial commit: Student Management System with OOP

--- dosomething2.py
+++
@@ -1,9 +0,0 @@
-def greet_person(name):
-    def greet():
-        print(f"hallo ich bin , {name}!")
-    return greet
-greet_Arif = greet_person("Arif")
-greet_Fatima = greet_person("Fatima")
-greet_Arif()
-greet_Fatima()
-
--- python01.py
+++
@@ -1,5 +0,0 @@
-def calculate_avarage(score):
-    return sum(score)/len(score)
-avg_1 = calculate_avarage([56,76,87,67,98,73]) 
-avg_2 = calculate_avarage([54,90,76,91,87,67,98,73])
-print(f"Avarage value of 01 ={avg_1:.2f},Avarage value of o2 = {avg_2: .3f}")
